074cd54d8462240bdc09da52fd39e155506ab764
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-06 22:58:16 +0300

    Add ContactSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import ProductsSection from '../components/ProductsSection';
 import BookingSection from '../components/BookingSection';
 import TestimonialsSection from '../components/TestimonialsSection';
 import FAQSection from '../components/FAQSection';
+import ContactSection from '../components/ContactSection';
 
 export default function HomePage() {
   return (
@@ -23,6 +24,7 @@ export default function HomePage() {
     <BookingSection />
     <TestimonialsSection />
     <FAQSection />
+    <ContactSection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
