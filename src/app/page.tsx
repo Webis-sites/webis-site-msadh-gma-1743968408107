95026f8fa55829a85a657048dd65fadb92fd0dcf
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-06 22:58:50 +0300

    Add Footer component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import BookingSection from '../components/BookingSection';
 import TestimonialsSection from '../components/TestimonialsSection';
 import FAQSection from '../components/FAQSection';
 import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
 
 export default function HomePage() {
   return (
@@ -25,6 +26,7 @@ export default function HomePage() {
     <TestimonialsSection />
     <FAQSection />
     <ContactSection />
+    <Footer />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
